039b3df77241f0be3c35b409b1f896be3b9c1a0a
Barry Allard <steakknife@users.noreply.github.com>
2015-05-27 20:16:30 -0700

    .app fix

--- lib/spirit_hands/prompt.rb
+++ lib/spirit_hands/prompt.rb
@@ -5,7 +5,7 @@ module SpiritHands
   module Prompt
     class << self
       def install!(app = nil)
-        ::SpiritHands.app ||= app
+        ::SpiritHands.app = ::Rails.application if defined?(Rails)
         ::Pry.config.prompt = [ main, multiline ].freeze
       end
     end
