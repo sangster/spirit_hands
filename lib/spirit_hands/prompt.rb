c01dfede15e1c0c36dae6420cb6af76d7003a46a
Barry Allard <steakknife@users.noreply.github.com>
2015-05-27 20:19:40 -0700

    better
    Signed-off-by: Barry Allard <barry.allard@gmail.com>

--- lib/spirit_hands/prompt.rb
+++ lib/spirit_hands/prompt.rb
@@ -5,7 +5,6 @@ module SpiritHands
   module Prompt
     class << self
       def install!(app = nil)
-        ::SpiritHands.app = ::Rails.application if defined?(Rails)
         ::Pry.config.prompt = [ main, multiline ].freeze
       end
     end
