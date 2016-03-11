79ded8ce190bfe1de52df78d6086171eb490a66b
Barry Allard <steakknife@users.noreply.github.com>
2016-03-10 22:16:17 -0800

    pry-rails fix: closes #8
    Signed-off-by: Barry Allard <barry.allard@gmail.com>

--- lib/spirit_hands/railtie.rb
+++ lib/spirit_hands/railtie.rb
@@ -1,6 +1,7 @@
 class Railtie < Rails::Railtie
   initializer 'spirit_hands.initialize' do |app|
     silence_warnings do
+      require 'pry-rails'
       ::SpiritHands.melody!(app)
     end
   end
