381c8662c69b6651495de0e09f27500e64cf15bb
Barry Allard <barry.allard@gmail.com>
2016-03-10 22:24:25 -0800

    require pry-rails much earlier
    Signed-off-by: Barry Allard <barry.allard@gmail.com>

--- lib/spirit_hands/railtie.rb
+++ lib/spirit_hands/railtie.rb
@@ -1,7 +1,6 @@
 class Railtie < Rails::Railtie
   initializer 'spirit_hands.initialize' do |app|
     silence_warnings do
-      require 'pry-rails'
       ::SpiritHands.melody!(app)
     end
   end
