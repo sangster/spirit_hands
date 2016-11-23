bbe113407df8171690611e6dc990e8377181a0dc
Barry Allard <barry.allard@gmail.com>
2016-11-23 05:46:48 -0800

    refactor coolline integration
    Signed-off-by: Barry Allard <barry.allard@gmail.com>

--- lib/spirit_hands/options/coolline.rb
+++ lib/spirit_hands/options/coolline.rb
@@ -1,63 +1,56 @@
 module SpiritHands
   class << self
-    # Is cooline enabled?
+    # Is coolline enabled?
     def coolline
       @coolline = DEFAULT_COOLLINE if @coolline.nil?
       @coolline
     end
 
     # Set whether hirb is enabled (default: true)
-    def coolline=(c)
-      c = DEFAULT_COOLLINE if c.nil?
-      @coolline = !!c
-    end
+    attr_writer :coolline
 
   protected
 
     def install_coolline!
       return if coolline_installed?
-      @orig_input ||= Pry.input
-      @orig_completer ||= Pry.config.completer
-      require 'pry-coolline'
       Pry.plugins['coolline'].activate!
-      begin
-        require 'pry-bond'
-        Pry.plugins['bond'].activate!
-      rescue LoadError
-      end
-      @coolline_input ||= Pry.input
-      @coolline_completer ||= Pry.config.completer
-      Pry.config.completer = @coolline_completer
-      Pry.input = @coolline_input
+      self.input_config = @coolline_input
     end
 
     def uninstall_coolline!
       return unless coolline_installed?
       Pry.plugins['coolline'].disable!
-      if !Pry.plugins['bond'].is_a? Pry::PluginManager::NoPlugin
-        Pry.plugins['bond'].disable!
-      end
-      Pry.config.completer = @orig_completer
-      Pry.input = @orig_input
+      self.input_config = @orig_input
     end
 
     def coolline_installed?
-      !@orig_input.nil? && @orig_input != Pry.input
+      Pry.input == @coolline_input[0]
     end
 
     def setup_coolline!
-      if SpiritHands.coolline
-        SpiritHands.install_coolline!
+      if coolline
+        install_coolline!
       else
-        SpiritHands.uninstall_coolline!
+        uninstall_coolline!
       end
     end
 
+    def input_config=(args)
+      Pry.input, Pry.config.completer = args
+    end
+
+    def input_config
+      [Pry.input, Pry.config.completer]
+    end
+
     def setup_coolline
-      Pry.hooks.add_hook(:after_read, "setup_coolline0") do |code, pry|
+      @orig_input = input_config
+      require 'pry-coolline'
+      @coolline_input = input_config
+      Pry.hooks.add_hook(:before_session, "setup_coolline_before") do |output, binding, pry|
         setup_coolline!
       end
-      Pry.hooks.add_hook(:before_session, "setup_coolline1") do |output, binding, pry|
+      Pry.hooks.add_hook(:after_eval, "setup_coolline_after") do |code, pry|
         setup_coolline!
       end
     end
